5f0d3701e4200654b9f28795499326690bb555e7
Vitor Barbosa <vitornga15@gmail.com>
2018-01-10 21:06:11 -0200

    :bulb: Add Number Wizard UI for Unity 5

---
+++ Unity 5.3.2f1/Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string game) {
+		Debug.Log ("Level requested for " + game);
+		SceneManager.LoadScene (game);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
---
+++ Unity 5.3.2f1/Number Wizard UI/Assets/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+	int max, min, guess;
+	int maxGuessesAllowed = 1000;
+
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame() {
+		max = 1000;
+		min = 1;
+		NextGuess ();
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+	
+	void NextGuess() {
+		guess = Random.Range(min, max + 1);
+		text.text = guess.ToString();
+		maxGuessesAllowed--;
+
+		if (maxGuessesAllowed <= 0) {
+			SceneManager.LoadScene ("Win");
+		}
+	}
+}
