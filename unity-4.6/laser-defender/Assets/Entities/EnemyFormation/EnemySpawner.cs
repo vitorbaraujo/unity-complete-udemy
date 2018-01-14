268665616a8e0a2423c548c1148482ba87f2364f
Vitor Barbosa <vitornga15@gmail.com>
2018-01-14 01:15:07 -0200

    :repeat: Automatically move enemy formation horizontally

--- unity-4.6/laser-defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
+++ unity-4.6/laser-defender/Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -4,17 +4,43 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 
 	public GameObject enemyPrefab;
+	public float width = 14f;
+	public float height = 7f;
+	public float speed = 5f;
 
-	// Use this for initialization
+	private float xmin;
+	private float xmax;
+	private bool movingRight;
+	
 	void Start(){
+		float distance = Mathf.Abs(transform.position.z - Camera.main.transform.position.z);
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint (new Vector3(0, 0, distance));
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint (new Vector3(1, 0, distance));
+		
+		xmin = leftMost.x;
+		xmax = rightMost.x;
+
 		foreach(Transform child in transform){
 			GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
 			enemy.transform.parent = child;
 		}
 	}
-	
-	// Update is called once per frame
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(transform.position, new Vector3(width, height));
+	}
+
 	void Update(){
-	
+		float leftEdgeOfFormation = transform.position.x - width / 2;
+		float rightEdgeOfFormation = transform.position.x + width / 2;
+		if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax) {
+			movingRight = !movingRight;
+		}
+
+		if (movingRight) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		} else {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}
 	}
 }
