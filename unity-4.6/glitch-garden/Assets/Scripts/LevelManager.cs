54979166d86929f30619e9220bd7a82f0e9d5dc1
Vitor Barbosa <vitornga15@gmail.com>
2018-01-17 13:32:24 -0200

    :bulb: Start Glitch Garden
    - Add splash screen
    - Add sound to splash screen
    - Automatically load menu after splash screen
    - Keep aspect ratio for images and texts for splash screen and menu

---
+++ unity-4.6/glitch-garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start() {
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string game) {
+		Debug.Log ("Level requested for " + game);
+		Application.LoadLevel (game);
+	}
+
+	public void LoadNextLevel() {
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+}
