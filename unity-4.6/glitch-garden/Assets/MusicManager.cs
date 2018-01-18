509e0e9b9571daa93776351a8eaf5c06d6bf8d06
Vitor Barbosa <vitornga15@gmail.com>
2018-01-18 08:00:59 -0200

    :repeat: Add Music Manager
    - Add stub for win, lose, options and level_01 levels

---
+++ unity-4.6/glitch-garden/Assets/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChangeArray;
+
+	private AudioSource audioSource;
+	
+	void Awake () {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnLevelWasLoaded(int level) {
+		AudioClip levelMusic = levelMusicChangeArray[level];
+		if (levelMusic) {
+			audioSource.clip = levelMusic;
+			audioSource.loop = true;
+			audioSource.Play();
+		}
+	}
+}
