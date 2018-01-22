d1ddc67f98bfbc2097224c00fde92407e2f90bc4
Vitor Barbosa <vitornga15@gmail.com>
2018-01-21 22:54:10 -0200

    :repeat: Add options sliders for volume and difficulty

--- unity-4.6/glitch-garden/Assets/Scripts/PlayerPrefsManager.cs
+++ unity-4.6/glitch-garden/Assets/Scripts/PlayerPrefsManager.cs
@@ -40,7 +40,7 @@ public class PlayerPrefsManager : MonoBehaviour {
 	}
 
 	public static void SetDifficulty(float difficulty) {
-		if (difficulty >= 0f && difficulty <= 1f) {
+		if (difficulty >= 1f && difficulty <= 3f) {
 			PlayerPrefs.SetFloat(DIFFICULTY_KEY, difficulty);
 		} else {
 			Debug.LogError("Difficulty out of range");
